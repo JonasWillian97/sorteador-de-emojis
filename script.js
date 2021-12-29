35712a53b6d0af7438ae172c4ce61adb175271ae
Jonas Willian <jwnarancity@gmail.com>
2021-12-29 11:29:33 -0300

    Add files via upload
    Sorteador de emojis feito com HTML, CSS e JS!
    Feito para fins de se desenvolver e por em prática conhecimentos aprendidos durante os estudos, além é claro de brincar um pouco rs.

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('#emoji');
+const emojis = [
+    "😃", "🤣", "🤩", "😘",
+    "😉", "😳", "🧐", "😯", 
+    "🥳", "😨", "😱", "🥶",
+    "🥵", "🤮", "🤒", "🥴"
+]
+btn.addEventListener("mouseover", () => {
+    btn.innerText = emojis[Math.floor(Math.random()*
+    emojis.length)]
+})
